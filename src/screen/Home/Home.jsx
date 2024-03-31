e5aa6b7fcd32853065845a0ec57064e156fea294
usman <mug832@gmail.con>
2024-03-31 11:20:36 +0500

    update

--- src/screen/Home/Home.jsx
+++ src/screen/Home/Home.jsx
@@ -8,14 +8,7 @@ import Footer from '../../components/Footer/Footer';
 const Home = () => {
     return (
         <div className={styles.homeContainer}>
-            <div className={styles.StyleBlock}>
-                <StyleBlock />
-            </div>
-            <div className={styles.StyleBlock2}>
-                <StyleBlock />
-            </div>
-            <div className={styles.square}></div>
-            <div className={styles.square2}></div>
+           
             <div style={{ position: 'sticky', top: 0, right: 0, backgroundColor: '#282C33' }}>
                 <Navbar />
             </div>
