917a04a57225918e7516c6a95b1d6929ed17ede6
Muhammad Usman <mug832@gmail.com>
2024-04-03 16:30:28 +0500

    today last task

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,32 +7,74 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className='footer_container'>
-            <div className="footer_sub_container1">
-                <div className="footer_profile">
-                    <div className="footer_profile_title">
-                        <img src="/footer-logo.png" alt="" />
-                        <p>Muhammad Usman</p>
-                        <span>mug832@gmail.com</span>
-                    </div>
-                    <p>Web designer and front-end developer</p>
-                </div>
-                <div className="footer_media">
-                    <h2>Media</h2>
-                    <div>
-                        <span><BsFacebook /></span>
-                        <span><FaInstagram /></span>
-                        <span><FaTwitterSquare /></span>
-                        <span><FaLinkedin /></span>
-                        <span><FaGithub /></span>
-                    </div>
-                </div>
+      <div className="footer_container">
+        <div className="footer_sub_container1">
+          <div className="footer_profile">
+            <div className="footer_profile_title">
+              <img src="/footer-logo.png" alt="" />
+              <p>Muhammad Usman</p>
+              <span>mug832@gmail.com</span>
             </div>
-            <div className="footer_sub_container2">
-                <p>© Copyright 2022. Made by Elias</p>
+            <p>Web designer and front-end developer</p>
+          </div>
+          <div className="footer_media">
+            <h2>Media</h2>
+            <div>
+              <span
+                onClick={() =>
+                  window.open(
+                    "https://www.facebook.com/profile.php?id=100041245301416",
+                    "_blank"
+                  )
+                }
+              >
+                <BsFacebook size={24} />
+              </span>
+              <span
+                onClick={() =>
+                  window.open(
+                    "https://www.instagram.com/muhammadusman1351/",
+                    "_blank"
+                  )
+                }
+              >
+                <FaInstagram size={24} />
+              </span>
+              <span
+                onClick={() =>
+                  window.open("https://twitter.com/Muhamma58869850", "_blank")
+                }
+              >
+                <FaTwitterSquare size={24} />
+              </span>
+              <span
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/muhammad-usman-profile121/",
+                    "_blank"
+                  )
+                }
+              >
+                <FaLinkedin size={24} />
+              </span>
+              <span
+                onClick={() =>
+                  window.open(
+                    "https://github.com/MuhammadUsmanGhani832",
+                    "_blank"
+                  )
+                }
+              >
+                <FaGithub size={24} />
+              </span>
             </div>
+          </div>
         </div>
-    )
+        <div className="footer_sub_container2">
+          <p>© Copyright 2022. @usman</p>
+        </div>
+      </div>
+    );
 }
 
 export default Footer
